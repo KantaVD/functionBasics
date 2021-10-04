9a3d2bf9d1fd3f10f17c8d913d6a8e3fa9da2d83
KantaVD <89583709+KantaVD@users.noreply.github.com>
2021-10-04 14:04:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const courgettePizzaMaken= function() {
+    console.log("Rasp 2 courgettes en doe de geraspte courgette in een zeef")
+    console.log("Voeg wat zout toe aan de courgettes en laat een uurtje staan")
+    console.log("Pers het overtollige water uit de courgette met de hand of gebruik kaasdoek")
+    console.log("Mix de courgette met kaas, ei en eventueel wat meel of bloem")
+    console.log("Spreid de courgettemix over een bakplaat")
+    console.log("Beleg je pizza met alles wat je lekker vindt")
+};
+
+const sushiMaken= function() {
+    console.log("Kook de rijst en laat afkoelen")
+    console.log("Pak een noriblad en leg er een  dunne laag rijst op, laat 1 cm over, druk dit goed aan")
+    console.log("Leg groente, vis of vlees in 1 lijn op de rijst")
+    console.log("Rol de sushi zo strak mogelijk op")
+    console.log("Plak het laatste randje nori met een beetje water vast")
+    console.log("Snijd de rol in stukjes van 2 cm breed")
+};
+
+sushiMaken();
+sushiMaken();
+sushiMaken();
+courgettePizzaMaken();
+courgettePizzaMaken();
